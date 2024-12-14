ba97e44e6e7c864f2a5fda1458cf097ba6f34150
MQ-06 <mariamqadeem181@gmail.com>
2024-12-15 00:40:34 +0500

    almost done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+ import React from "react";
 import { FaShoppingCart } from "react-icons/fa";
 
 const Header = ({ cartItems, toggleSidebar }) => {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+ import React from "react";
 import Header from "./Header";
 import necklace from "../assets/necklaces.jpg";
 import person from "../assets/a girl.jpg";
